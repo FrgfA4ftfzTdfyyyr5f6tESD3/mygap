8166ea58b17e19266bba22e48cefd51ad906d4c8
FrgfA4ftfzTdfyyyr5f6tESD3 <119043966+FrgfA4ftfzTdfyyyr5f6tESD3@users.noreply.github.com>
2026-06-23 01:18:04 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,2 @@
 #!/bin/bash
-
-echo "🚀 Starting ArgoSBX Container for Railway..."
-
-# پاک کردن اتوماتیک کاراکترهای ویندوز به محض روشن شدن کانتینر
-sed -i 's/\r$//' /root/argosbx.sh
-
-# تبدیل متغیرهای محیطی ریلوی به متغیرهای مورد نیاز اسکریپت
-export uuid="${uuid}"
-export vmpt="${vmpt:-8080}"
-export argo="${argo:-vmpt}"
-export sub="${sub:-y}"
-export subpt="${PORT:-3000}"
-
-echo "Executing main script..."
-bash /root/argosbx.sh
-
-echo "Container is alive. Streaming logs..."
-sleep 5
-tail -f /root/agsbx/argo.log 2>/dev/null || tail -f /Dev/null
+sed -i 's/\r$//' /root/argosbx.sh && export uuid="${uuid}" vmpt="${vmpt:-8080}" argo="${argo:-vmpt}" sub="${sub:-y}" subpt="${PORT:-3000}" && bash /root/argosbx.sh
